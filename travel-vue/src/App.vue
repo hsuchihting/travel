fe433a236dc6e1de8bb859476f4d86bf26a1e87c
Tim Hsu <tim.hsu@tpisoftware.com>
2022-07-03 23:30:54 +0800

    vue

---
+++ travel-vue/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="text-center bg-gray-100 p-5">
+    <p class="text-6xl text-red-700">Hello!</p>
+    <h1 class="text-4xl text-green-500">Vite + Vue + TailwindCSS</h1>
+  </div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ travel-vue/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "./style/index.css"; //在此引入
+
+createApp(App).mount('#app')
