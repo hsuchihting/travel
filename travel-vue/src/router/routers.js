be0d72f887eea9a84d940d29b76fd3ec2007cb2e
Tim Hsu <tim.hsu@tpisoftware.com>
2022-07-04 23:44:13 +0800

    router add

---
+++ travel-vue/src/router/routers.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import locations from "../views/locations";
+import foods from "../views/foods";
+import hotels from "../views/hotels";
+
+const routes = [
+  {
+    path: "/locations",
+    name: "locations",
+    component: locations,
+  },
+  {
+    path: "/foods",
+    name: "foods",
+    component: foods,
+  },
+  {
+    path: "/hotels",
+    name: "hotels",
+    component: hotels,
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
---
+++ travel-vue/src/views/locations.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>locations working</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
