d7f56435c8b0076857c75fcc90eccdeece3f9e99
Tim Hsu <tim.hsu@tpisoftware.com>
2022-07-10 13:24:47 +0800

    正確安裝 tailwind 3

--- travel-vue/tailwind.config.js
+++ travel-vue/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
